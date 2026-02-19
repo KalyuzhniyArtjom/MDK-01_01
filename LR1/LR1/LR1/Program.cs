8887e52a9044c91f2f55f208c843a25ab65748a2
qwizza <meowmeowpiupiu@mail.ru>
2026-02-19 12:25:40 +0300

    Сделал практическую 1.1

---
+++ LR1/LR1/LR1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество элементов массива: ");
+            int Lenght = Convert.ToInt32(Console.ReadLine());
+            int[] Array = new int[Lenght];
+            for (int index = 0; index < Lenght; index++)
+            {
+                Console.Write($"Элемент [{index}]: ");
+                Array[index] = Convert.ToInt32(Console.ReadLine());
+            }
+            int sum = 0;
+            double average = 0.0;
+            int count = 0;
+            foreach (int element in Array)
+            {
+                sum += element;
+            }
+            average = sum / Lenght;
+            foreach (int element in Array)
+            {
+                if (element < average)
+                {
+                    ++count;
+                }
+            }
+            Console.WriteLine($"Среднее значение массива: {average}");
+            Console.WriteLine($"Количество элементов, меньших среднего значения: {count} ");
+        }
+    }
+}
