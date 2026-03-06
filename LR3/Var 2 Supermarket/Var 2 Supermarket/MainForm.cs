220a47907cc2e8687e181c911a80293bb77c550b
Gavrilchenko-Stepan <atom47228@gmail.com>
2026-03-06 11:03:04 +0300

    сделал сценарии

--- LR3/Var 2 Supermarket/Var 2 Supermarket/MainForm.cs
+++ LR3/Var 2 Supermarket/Var 2 Supermarket/MainForm.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Var_2_Supermarket
@@ -19,15 +14,15 @@ namespace Var_2_Supermarket
 
             products_.Add("Кондитерские изделия", new List<Product>()
             {
-                new Product("Печенье Юбилейное", 50.50, "Юбиленое", new DateTime(2026, 3, 5), "Вкусное печенье", "D:\\VS studio\\LR3\\image\\Печенье.png"),
-                new Product("Молочный ломтик", 75.00, "Завод киндер", new DateTime(2026, 2, 15), "Нежная вкуснятина", "D:\\VS studio\\LR3\\image\\Ломтик.jpg"),
+                new Product("Печенье Юбилейное", 50.50, "Юбиленое", new DateTime(2026, 3, 5), "Вкусное печенье", "C:\\repp\\LR3\\image\\Печенье.png"),
+                new Product("Молочный ломтик", 75.00, "Завод киндер", new DateTime(2026, 2, 15), "Нежная вкуснятина", "C:\\repp\\LR3\\image\\Ломтик.jpg"),
                 
             }
         );
             products_.Add("Напитки", new List<Product>()
             {
-                new Product("Кока-Кола", 100.00, "Кока-Кола", new DateTime(2026, 4, 20), "Хорошо для праздника", "D:\\VS studio\\LR3\\image\\Кола.jpg"),
-                new Product("Колокольчик", 45.00, "Фруто", new DateTime(2026, 2, 23), "Вкус детства", "D:\\VS studio\\LR3\\image\\Колокольчик.jpg"),
+                new Product("Кока-Кола", 100.00, "Кока-Кола", new DateTime(2026, 4, 20), "Хорошо для праздника", "C:\\repp\\LR3\\image\\Кола.jpg"),
+                new Product("Колокольчик", 45.00, "Фруто", new DateTime(2026, 2, 23), "Вкус детства", "C:\\repp\\LR3\\image\\Колокольчик.jpg"),
             }
         );
             List<string> allCategories = products_.Keys.ToList();
